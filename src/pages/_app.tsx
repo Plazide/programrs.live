68afb68d67e5cd989ec0b537516e30f50e68b1cb
Carl Hallén Jansson <callehj@hotmail.se>
2021-05-28 18:47:10 +0200

    Add plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
 import { Provider } from "urql";
 import client from "../util/urql";
+import PlausibleProvider from "next-plausible";
 
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Provider value={client}>
-		<Component {...pageProps} />
+		<PlausibleProvider domain="programrs.live">
+			<Component {...pageProps} />
+		</PlausibleProvider>
 	</Provider>
 }
 
