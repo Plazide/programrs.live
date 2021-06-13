2faa608f57b59624182926a48761f762fa1f93a4
Carl Hallén Jansson <callehj@hotmail.se>
2021-06-13 13:01:31 +0200

    Add logo and title to header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactElement, ReactNode } from "react"
+import Logo from "../../images/logo.svg";
 
 import styles from "./layout.module.css";
 
@@ -9,7 +10,12 @@ interface Props {
 export default function Layout({ children }: Props): ReactElement {
 	return (
 		<>
-			<header className={styles.header}></header>
+			<header className={styles.header}>
+				<div className={styles.content}>
+					<Logo className={styles.logo} />
+					<span className={styles.title}>Programrs</span>
+				</div>
+			</header>
 			<main className={styles.main}>{children}</main>
 		</>
 	)
