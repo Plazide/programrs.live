9d7ad95ca30612042e0b8c87398db5ee752f2864
Carl Hallén Jansson <callehj@hotmail.se>
2021-06-13 12:45:42 +0200

    Add blocklist

---
+++ src/models/blocklist.ts
@@ -0,0 +1,19 @@
+/* 
+	This file contains blocked words and channels. These words or channels are usually blocked because they stream content that is not programming related, but are still listed under programming categories on the various services.
+
+	Channels do not need to have proper capitalization, all checks will be made in a case-insensitive way.
+*/
+
+export default {
+	words: [
+		// Add blocked words here...
+	],
+	channels: [
+		"LabPadre",
+		"cnbc awaaz",
+		"pearl tv",
+		"piangper man",
+		"pixcams",
+		"chryston parish church"
+	]
+}
--- src/pages/api/streams.ts
+++ src/pages/api/streams.ts
@@ -1,20 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { fetchFromTwitch } from "../../util/api/twitch";
-import { saveToFauna } from "../../util/api/fauna"
+import { saveToFauna } from "../../util/api/fauna";
+import blocked from "../../models/blocklist";
 
 // Types
 import { NormalizedStream } from "../../types/streams";
 import { fetchFromYoutube } from "../../util/api/youtube";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {	
-	const streams: NormalizedStream[][] = await Promise.all([
+	const raw: NormalizedStream[][] = await Promise.all([
 		fetchFromTwitch(),
 		fetchFromYoutube()
 	]);
 
-	await saveToFauna(streams.flat())
+	const flattened = raw.flat();
+	console.log(flattened.length);
+	const streams = filterBlocked(flattened, blocked);
+	console.log(streams.length);
+	await saveToFauna(streams)
 
 	res.statusCode = 200
 	res.end();
+}
+
+function filterBlocked(streams: NormalizedStream[], blocklist: typeof blocked): NormalizedStream[]{
+	const blockedChannels = blocklist.channels.map( channel => channel.toLocaleLowerCase());
+	const blockedWords = blocklist.words.map( word => new RegExp(`.*${word}.*`, "i"));
+
+	console.log(blockedChannels)
+
+	return streams.filter( stream => {
+		const containsBlockedWord = blockedWords.some( word => word.test(stream.title));
+
+		if(blockedChannels.includes(stream.channel.toLocaleLowerCase())) return false;
+		if(containsBlockedWord) return false;
+
+		return true;
+	})
 }
