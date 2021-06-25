ed5203eebd283d20ca5921ce6d7b5eee41b81c0a
Carl Hallén Jansson <callehj@hotmail.se>
2021-06-25 15:48:29 +0200

    Try to reduce amount of write ops

--- src/util/api/fauna.ts
+++ src/util/api/fauna.ts
@@ -14,56 +14,78 @@ const client = new Client({
 
 export async function saveToFauna(streams: NormalizedStream[]){
 	return client.query(
-		q.Do(
-			q.Let(
-				{
-					documents: q.Match(
-						q.Index("list-streams")
-					)
-				},
-				q.Foreach(
-					q.Paginate(q.Var("documents"), { size: 10000 }),
-					q.Lambda(
-						"ref",
-						q.Delete(q.Var("ref"))
-					)
+		q.Let(
+			{
+				documents: q.Match(
+					q.Index("list-streams")
 				)
-			),
-			q.Foreach(
-				streams,
-				q.Lambda(
-					"stream",
-					q.Let(
-						{
-							match: q.Match(
-								q.Index("stream-by-channel"),
-								q.Select(["channel"], q.Var("stream"))
-							)
-						},
-						q.If(
-							q.Exists(q.Var("match")),
-							q.Update(
-								q.Select("ref", q.Get(q.Var("match"))), {
+			},
+			q.Do(
+				q.Foreach(
+					streams,
+					q.Lambda(
+						"stream",
+						q.Let(
+							{
+								match: q.Match(
+									q.Index("stream-by-channel"),
+									q.Select(["channel"], q.Var("stream"))
+								)
+							},
+							q.If(
+								q.Exists(q.Var("match")),
+								q.Update(
+									q.Select("ref", q.Get(q.Var("match"))), {
+										data: q.Merge(
+											q.Var("stream"),
+											{
+												startedAt: q.Time(q.Select("startedAt", q.Var("stream")))
+											}
+										)
+									}
+								),
+								q.Create(q.Collection("Stream"), {
 									data: q.Merge(
 										q.Var("stream"),
 										{
 											startedAt: q.Time(q.Select("startedAt", q.Var("stream")))
 										}
 									)
-								}
-							),
-							q.Create(q.Collection("Stream"), {
-								data: q.Merge(
-									q.Var("stream"),
-									{
-										startedAt: q.Time(q.Select("startedAt", q.Var("stream")))
-									}
+								})
+							)
+						)
+					)
+				),
+				q.Foreach(
+					q.Map(
+						q.Paginate(q.Var("documents"), { size: 10_000 }),
+						q.Lambda(
+							"ref",
+							q.Get(q.Var("ref"))
+						)
+					),
+					q.Lambda(
+						"doc",
+						q.If(
+							q.Any(
+								q.Map(
+									streams,
+									q.Lambda(
+										"stream",
+										q.Equals(
+											q.Select(["channel"], q.Var("stream")),
+											q.Select(["data", "channel"], q.Var("doc"))
+										)
+									)
 								)
-							})
+							),
+							null,
+							q.Delete(
+								q.Select("ref", q.Var("doc"))
+							)
 						)
 					)
 				)
-				
 			)
 		)
 	)
